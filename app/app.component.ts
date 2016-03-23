ecf0021379b5c33f980f82812a6b80b08c3e6942
taylor pokoj and tyler suderman <pokojt@gmail.com>
2016-03-23 09:27:11 -0700

    add list of current kegs

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from 'angular2/core';
+import { Component} from 'angular2/core';
+// import { TaskListComponent } from './task-list.component';
+import { Keg } from './keg.model';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>Tap Room App</h1>
+      <div class="kegItem"  *ngFor="#keg of kegs">
+        <h3>{{ keg.name }}</h3>
+        <p>{{ keg.brand }}</p>
+        <p>{{ keg.price }}</p>
+        <p>{{ keg.abv }}</p>
+      </div>
     </div>
   `
 })
 
 export class AppComponent {
-
+  public kegs: Keg[];
+  constructor() {
+    this.kegs = [
+      new Keg("Blonde IPA", "Pfriem Family Brewers", 6, 7.5),
+      new Keg("Dead Guy Ale", "Rogue", 5, 6.8),
+      new Keg("Ripe", "Great Notion Brewing", 6, 7.2),
+      new Keg("PBR", "Pabst Brewing Company", 2, 4.5)
+    ];
+  }
+  kegWasSelected(clickedKeg: Keg): void {
+    console.log("parent", clickedKeg);
+  }
 }
